fa950b6376bef21e4ecbac1a5b4a50d0d4643555
dimah <dima.haker007@gmail.com>
2025-11-29 11:38:05 +0200

    моделі для виведення контролерів

---
+++ Core/metods/DTO.cs
@@ -0,0 +1,19 @@
+﻿public class UserDto
+{
+    public int id { get; set; }
+    public string name { get; set; }
+}
+
+public class MusicDto
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string artist { get; set; }
+    public string song_LONGELOS { get; set; }
+}
+
+public class UserLoginDto
+{
+    public string name { get; set; }
+    public string password { get; set; }
+}
