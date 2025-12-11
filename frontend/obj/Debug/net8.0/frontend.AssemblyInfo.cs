75fddfae868783488fcd289eeb53da22654aaa84
dimah <dima.haker007@gmail.com>
2025-12-12 01:08:30 +0200

    добавлення нової сторінки блейзор для створення акаунтів

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa888127a0e2487eaf11dd3f3f70ca421dffc97d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866482f359bb2a0e083f043ff73374398f2eb374")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
