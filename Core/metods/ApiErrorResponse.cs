9e6f88b04fea4c9291968c78d3db75e0564dedb0
dimah <dima.haker007@gmail.com>
2025-12-22 15:06:10 +0200

    лаба 5

---
+++ Core/metods/ApiErrorResponse.cs
@@ -0,0 +1,7 @@
+﻿public class ApiErrorResponse
+{
+    public string Error { get; set; } = string.Empty;
+    public string? Code { get; set; }
+    public string? Details { get; set; }
+    public string RequestId { get; set; } = string.Empty;
+}
